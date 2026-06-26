ffb4059257a0d4d4b115f880034b2cc3a5f82dda
tarannum18 <tarannum@emaad-infotech.com>
2026-06-26 16:53:30 +0530

    y project

---
+++ script.js
@@ -0,0 +1,76 @@
+let scan=document.getElementById("scan")
+
+let preview=document.getElementById("preview")
+
+let result=document.getElementById("result")
+
+let mark=document.getElementById("mark")
+
+let bar=document.getElementById("bar")
+
+let status=document.getElementById("status")
+
+scan.addEventListener("change",()=>{
+
+let file=scan.files[0]
+
+if(file){
+
+preview.src=
+URL.createObjectURL(file)
+
+result.innerText=""
+
+mark.style.display="none"
+
+bar.style.width="0%"
+
+status.innerText=
+"Scan uploaded"
+
+}
+
+})
+
+function analyze(){
+
+if(preview.src==""){
+
+alert(
+"Please upload brain scan"
+)
+
+return
+
+}
+
+status.innerText=
+"Analyzing..."
+
+let width=0
+
+let interval=setInterval(()=>{
+
+width+=5
+
+bar.style.width=
+width+"%"
+
+if(width>=100){
+
+clearInterval(interval)
+
+mark.style.display=
+"block"
+
+status.innerText=
+"Completed"
+
+result.innerText=
+"Abnormal Region Detected"
+
+}
+
+},100)
+
+}
